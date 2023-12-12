c5ece8a127cdbd7d0ac64152bac23725145c7e1a
ernestovv85 <ernestovv85@gmail.com>
2023-12-12 13:16:30 -0600

    Added currency format helper

--- components/Product.js
+++ components/Product.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { currencyFormat } from "../helpers"
 
 export const Product = ({product}) => {
   const {name, price, image} = product
@@ -7,7 +8,9 @@ export const Product = ({product}) => {
       <Image src={`/assets/img/${image}.jpg`} alt={name} width={400} height={500}/>
       <div className="p-5">
         <h3 className="text-2xl font-bold">{name}</h3>
-        <p></p>
+        <p className="mt-5 font-black text-4xl text-amber-400">
+          {currencyFormat(price)}
+        </p>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
-import { Layout } from "../layout/Layout";
-import { useQuiosco } from "../hooks/useQuiosco";
-import { Product } from "../components/Product";
-
+import { Layout } from '../layout/Layout'
+import { useQuiosco } from '../hooks/useQuiosco'
+import { Product } from '../components/Product'
 
 export default function Home() {
-  const {currentCategory} = useQuiosco()
+  const { currentCategory } = useQuiosco()
   return (
     <div>
       <Layout page={`Menú ${currentCategory.name}`}>
-        <h1 className="text-4xl font-black">{currentCategory?.name}</h1>
-        <p className="text-2xl my-10">Elige el contenido de tu pedido</p>
-        {currentCategory?.products?.map(product => (
-          <Product key={product.id} product={product}/>
-        ))}
+        <h1 className='text-4xl font-black'>{currentCategory?.name}</h1>
+        <p className='text-2xl my-10'>Elige el contenido de tu pedido</p>
+        <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+          {currentCategory?.products?.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
       </Layout>
     </div>
   )
